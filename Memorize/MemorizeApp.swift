1f44d4572c9ad1a76da60340916f3d70290f6e1d
Ameet Arurkar <ameet@ubidesk.com>
2024-11-10 20:55:16 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ameet Arurkar on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
